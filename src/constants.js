849cd4fa7322a9c7eb93be145e7ccb4f336c2ab9
Fahell <32997538+Fahell@users.noreply.github.com>
2026-05-28 17:18:02 -0300

    fix: Cannon-es isolated renderer (#28)
    * fix: Cannon-es isolated renderer instead of shared Three.js scene
    
    * fix: remove rendererData param from Cannon-es initPhysics3D call
    
    * chore: release v1.7.3

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // src/constants.js
 // Constantes globais do projeto
 
-export const VERSION = 'v1.7.2';
+export const VERSION = 'v1.7.3';
 export const CDN_BASE = `https://cdn.jsdelivr.net/gh/Fahell/test-perchance-git@${VERSION}`;
 export const BUNDLE_PATH = `${CDN_BASE}/dist/main.bundle.js`;
