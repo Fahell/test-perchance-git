59054c3935b455e262cac74ddf478c30aeb5901e
Fahell <rafaeltavares237@gmail.com>
2026-05-29 18:34:04 -0300

    fix: optimize particles for web browsers (v1.8.4)

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // src/constants.js
 // Constantes globais do projeto
 
-export const VERSION = 'v1.8.3';
+export const VERSION = 'v1.8.4';
 export const CDN_BASE = `https://cdn.jsdelivr.net/gh/Fahell/test-perchance-git@${VERSION}`;
 export const BUNDLE_PATH = `${CDN_BASE}/dist/main.bundle.js`;
