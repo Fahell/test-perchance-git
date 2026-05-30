44a44d053dae34752afd495b6fffe1a6e2c47e17
Fahell <rafaeltavares237@gmail.com>
2026-05-29 22:32:16 -0300

    chore: release v1.13.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Test Perchance Git (v1.12.3)
+# Test Perchance Git (v1.13.0)
 
 Projeto de teste para explorar as capacidades do Perchance com arquitetura modular usando ES6 Modules + GitHub + jsDelivr CDN.
 
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // src/constants.js
 // Constantes globais do projeto
 
-export const VERSION = 'v1.12.3';
-export const CDN_BASE = `https://cdn.jsdelivr.net/gh/Fahell/test-perchance-git@v1.12.3`;
+export const VERSION = 'v1.13.0';
+export const CDN_BASE = `https://cdn.jsdelivr.net/gh/Fahell/test-perchance-git@v1.13.0`;
 export const BUNDLE_PATH = `${CDN_BASE}/dist/main.bundle.js`;
