2395b235fed92943fa2a8be9f3d147bdbe3b8b16
Fahell <rafaeltavares237@gmail.com>
2026-05-31 18:07:10 -0300

    chore: release v1.17.6

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -34,8 +34,8 @@ const bridgeMod = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.definePro
   image,
   root
 }, Symbol.toStringTag, { value: "Module" }));
-const VERSION = "v1.17.5";
-const CDN_BASE = `https://cdn.jsdelivr.net/gh/Fahell/test-perchance-git@v1.17.5`;
+const VERSION = "v1.17.6";
+const CDN_BASE = `https://cdn.jsdelivr.net/gh/Fahell/test-perchance-git@v1.17.6`;
 function initRenderer(container2) {
   console.log("🎨 [Renderer] Inicializando Three.js...");
   const existingCanvas = document.querySelector('canvas[data-threejs="true"]');
