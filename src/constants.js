0b6614dca25f6d955d529e3a4d6894909b6897fe
Fahell <32997538+Fahell@users.noreply.github.com>
2026-06-07 08:56:02 -0300

    chore: release v1.28.0

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // src/constants.js
 // Constantes globais do projeto
 
-export const VERSION = 'v1.27.3';
-export const CDN_BASE = `https://cdn.jsdelivr.net/gh/Fahell/test-perchance-git@v1.27.3`;
+export const VERSION = 'v1.28.0';
+export const CDN_BASE = `https://cdn.jsdelivr.net/gh/Fahell/test-perchance-git@v1.28.0`;
 export const BUNDLE_PATH = `${CDN_BASE}/dist/main.bundle.js`;
